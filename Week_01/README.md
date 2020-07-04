72c09d20cc7cc6c89ef188d95f861f1bc9c6ac4f
allen <961105114@qq.com>
2020-07-04 22:57:36 +0800

    第一周作业

--- Week_01/README.md
+++ Week_01/README.md
@@ -1,1 +1,32 @@
-学习笔记
+## Queue
+   - 队列是一种比较特殊的线性结构。
+  -  它只允许在表的前端（front）进行删除操作，而在表的后端（rear）进行插入操作。
+  - 队列中最先插入的元素也将最先被删除，对应的最后插入的元素将最后被删除。
+  - 队列又称为“先进先出”（FIFO—first in first out）的线性表，与栈(FILO-first in last out)相反
+###  接口方法
+    boolean add(E e)
+    向队列中添加元素
+    E element()
+    返回队列的头，且不移除
+    boolean offer(E e)
+    向队列中添加元素
+    E peek()
+    返回队列的头，且不移除
+    E poll()
+    返回队列的头，且移除
+    remove()
+    返回队列的头，且移除
+##  Stack
+ - 限制仅在表的一端进行插入和删除运算的线性表。
+ - 栈为后进先出（Last In First Out）的线性表，简称为LIFO表。
+ ###  接口方法
+     boolean empty()
+     判断栈是否为空
+     E peek()
+     返回栈顶对象，不移除
+     E pop()
+     返回栈顶对象，并移除
+     E push(E item)
+     压入栈顶
+     int search(Object o)
+     返回对象在栈的位置
---
+++ Week_01/两数之和.java
@@ -0,0 +1,81 @@
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//
+//
+//
+// 示例:
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+//
+// Related Topics 数组 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     *  数组遍历两次计算
+     *   时间复杂度 O(n²)
+     *   空间复杂度 O(1)
+     * @param nums  整数数组
+     * @param target 目标值
+     * @return 数组中和为目标值的两个整数的下标
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1 ; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     *  两遍哈希表
+     *  时间复杂度 O(n)
+     *  空间复杂度 O(n)
+     * @param nums  整数数组
+     * @param target 目标值
+     * @return 数组中和为目标值的两个整数的下标
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result) && i != map.get(result)) {
+                return new int[] {i, map.get(result)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    /**
+     *  一遍哈希表
+     *  时间复杂度 O(n)
+     *  空间复杂度 O(n)
+     * @param nums  整数数组
+     * @param target 目标值
+     * @return 数组中和为目标值的两个整数的下标
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int result = target -nums[i];
+            if (map.containsKey(result)) {
+                return new int[] {map.get(result), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
